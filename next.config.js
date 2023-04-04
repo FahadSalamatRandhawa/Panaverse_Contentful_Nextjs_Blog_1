7f07770f7ed794d9ff6cfbfd3d7b2696ff8279ce
FahadSalamatRandhawa <dreamtwister61814@gmail.com>
2023-04-05 00:52:59 +0500

    blog codded

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  env:{
+    CONTENTFUL_SPACE_ID:'ij92yfbun5y2',
+    CONTENTFUL_ACCESS_KEY:'OG_tB8bc4dOuIFEGUDaEtYzgy3N51clcolwTWIJXsmk'
+  },
+  images:{
+    remotePatterns:[
+      {
+        protocol:'https',
+        hostname:'images.ctfassets.net',
+        port:''
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
